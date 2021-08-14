ad2cc1d8282a769e6f9d543729223e7527ed52de
soongjamm <kimstz0303@gmail.com>
2021-08-14 11:31:29 +0900

    init

---
+++ src/main/java/com/soongjamm/techlab/TechLabApplication.java
@@ -0,0 +1,13 @@
+package com.soongjamm.techlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechLabApplication.class, args);
+	}
+
+}
